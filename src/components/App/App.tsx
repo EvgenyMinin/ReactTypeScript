3c559dcc00b0e9da4e00970ac55b5211eb710a94
Johnny1992 <evgeny.frontend@gmail.com>
2020-02-17 20:00:10 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./styles.sass";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="header">Hello</h1>
+    </div>
+  );
+};
+
+export default App;
