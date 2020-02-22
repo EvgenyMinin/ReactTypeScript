7e1b58306268fe060268ac77143fadfe56d0aed3
Johnny1992 <evgeny.frontend@gmail.com>
2020-02-22 14:04:02 +0300

    added routing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,37 @@
 import React from "react";
+import { Link, Router, RouteComponentProps } from "@reach/router";
+import News from "../News/News";
 
-import "./styles.sass";
+import "./styles.scss";
 
-const App = () => {
+interface IAppProps extends RouteComponentProps {
+  name: string;
+  site: string;
+}
+
+const App: React.FC<IAppProps> = props => {
   return (
-    <div>
-      <h1 className="header">Hello</h1>
+    <div className="App">
+      <h1 className="App__header">Project with hooks and TypeScript</h1>
+      <nav>
+        <Link to="/">Home</Link> <Link to="news">News</Link>
+      </nav>
+      <hr />
+      <p>
+        Автор: {props.name} | Сайт: {props.site}
+      </p>
+      <hr />
+      {props.children}
     </div>
   );
 };
 
-export default App;
+const RouterApp = () => (
+  <Router>
+    <App path="/" name="John" site="johnfrontend.ru">
+      <News path="/news" />
+    </App>
+  </Router>
+);
+
+export default RouterApp;
---
+++ src/components/News/News.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { RouteComponentProps } from "@reach/router";
+
+const News: React.FC<RouteComponentProps> = () => (
+  <div>
+    <p>News</p>
+  </div>
+);
+
+export default News;
