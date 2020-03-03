d5fe46078221799d9c8da206bbda8e81a933327d
Johnny1992 <evgeny.frontend@gmail.com>
2020-03-03 22:39:24 +0300

    api for login

---
+++ src/api/auth.ts
@@ -0,0 +1,39 @@
+import { navigate } from "@reach/router";
+import { IUserIdentity } from "../models/user";
+
+interface IAuthResponse {
+  status: number;
+  data?: any;
+  errorText?: string;
+}
+
+const checkCredentials = (data: IUserIdentity): Promise<IAuthResponse> => {
+  const promise = new Promise<IAuthResponse>((resolve, reject) => {
+    if (!checkCredentials(data)) {
+      reject({
+        status: 500,
+        errorText: "incorrect_login_or_password"
+      });
+    }
+    window.localStorage.setItem("tstz.authenticated", "true");
+    resolve({
+      status: 200,
+      data: "ok"
+    });
+  });
+
+  return promise;
+};
+
+export const checkAuthStatus = (): boolean => {
+  if (localStorage.getItem("tstz.authenticated")) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const logout = (): void => {
+  window.localStorage.removeItem("tstz.authenticated");
+  navigate("/");
+};
